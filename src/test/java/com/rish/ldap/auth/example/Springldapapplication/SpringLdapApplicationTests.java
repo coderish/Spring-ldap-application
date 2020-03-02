e073b4fb709b5acacda64cd3c3f196f16b7e7d93
Rishabh Sharma <rishabh.sharma476@yahoo.com>
2020-03-02 13:51:28 +0400

    code commit

---
+++ src/test/java/com/rish/ldap/auth/example/Springldapapplication/SpringLdapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rish.ldap.auth.example.Springldapapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringLdapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
